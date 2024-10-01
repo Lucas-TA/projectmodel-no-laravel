13b2e6059220e5505239bdc4a2976265529c9da7
Lucas-TA <dev.ascencao@gmail.com>
2024-10-01 16:15:10 -0300

    meerging to main

--- application/views/default.php
+++ application/views/default.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?php echo $this->e($title); ?></title>
         <link rel="stylesheet" href="/assets/css/styles.css">
-        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.1/dist/cdn.min.js"></script>
     </head>
     <body>
         <div class="header container">
--- application/views/home.php
+++ application/views/home.php
@@ -1,6 +1,5 @@
 <?php $this->layout('default', ['title' => $title]) ?>
 <h2> Users </h2>
-
 <ul class="users-home">
     <?php foreach($users as $user): ?>
         <li><?php echo $user->firstName ?> | <a href="/user/<?php echo $user->id; ?>">Details</a> </li>
